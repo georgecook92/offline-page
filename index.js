6a5ba3657f193e24113aaaf15bc0f27b2b99fedb
George <george.cook.92@hotmail.co.uk>
2016-05-31 11:12:41 +0100

    simplify SW code - rremove activate event and simplify fetch event

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var app = express();
-var path    = require("path");
+var path = require("path");
 
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/', express.static(__dirname + '/'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname+'/public/index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.get('/anotherPage', function (req, res) {
-  res.sendFile(path.join(__dirname+'/public/page.html'));
+app.get('/anotherPage', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/page.html'));
 });
 
- 
-app.listen(3000, function () {
+
+app.listen(3000, function() {
     console.log("Listening on port 3000");
 });

481747f18f32599d3b2da7468d9fad32259ee29f
George <george.cook.92@hotmail.co.uk>
2016-06-07 21:22:43 +0100

    adding read me

--- sw.js
+++ sw.js
@@ -27,31 +27,7 @@ self.addEventListener('fetch', function(event) {
                 console.log("Return response from cache");
                 return response;
             }
-            /*
-                    //stream can only be used once so needs to be cloned
-                    var fetchRequest = event.request.clone();
-
-                    return fetch(fetchRequest).then(
-                      function(response) {
-                        // Check if we received a valid response
-                        if(!response || response.status !== 200 || response.type !== 'basic') {
-                          return response;
-                        }
-
-                        // IMPORTANT: Clone the response. A response is a stream
-                        // and because we want the browser to consume the response
-                        // as well as the cache consuming the response, we need
-                        // to clone it so we have 2 streams.
-                        var responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                          .then(function(cache) {
-                            cache.put(event.request, responseToCache);
-                          });
-
-                        return response;
-                      }
-                    ); */
+            
         })
     );
 });
